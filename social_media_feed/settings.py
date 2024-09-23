c21e4b6fe2aeab902361b88ce67f6ab45aa07052
roicy123 <041129roycy@gmail.com>
2024-09-23 12:46:17 +0530

    loadbalancer

--- social_media_feed/settings.py
+++ social_media_feed/settings.py
@@ -90,17 +90,27 @@ WSGI_APPLICATION = 'social_media_feed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'social_db',
+#         'USER': 'roicy',
+#         'PASSWORD': '2910',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_db',
-        'USER': 'roicy',
-        'PASSWORD': '2910',
-        'HOST': 'localhost',
+        'NAME': 'backendcourse',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'backendcourse.c5wgwi4qg9mg.eu-north-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 
 
 # Password validation

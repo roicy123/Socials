36265eb12642a736c1539e3eab1f434c441b306e
roicy123 <041129roycy@gmail.com>
2024-10-02 13:51:01 +0530

    Jenkins files add

--- social_media_feed/settings.py
+++ social_media_feed/settings.py
@@ -154,6 +154,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
